631bb15b47bdaba18f2e7e2ee2350fe31d069e06
Daniel Humphreys <danielhumphreys@live.ca>
2022-02-24 13:48:19 -0500

    3 b) Not completed but attempted

--- src/main/java/com/example/st200352106test2/PokedexController.java
+++ src/main/java/com/example/st200352106test2/PokedexController.java
@@ -5,6 +5,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class PokedexController implements Initializable {
@@ -56,6 +57,7 @@ public class PokedexController implements Initializable {
         user.addPokemon(pokemon9);
         user.addPokemon(pokemon10);
 
+
         nameLabel.setText(pokemon1.getName());
         numberLabel.setText(String.valueOf(pokemon1.getPokemonNumber()));
         genderLabel.setText(pokemon1.getGender());
@@ -65,9 +67,44 @@ public class PokedexController implements Initializable {
 
         numberOfPokemonLabel.setText("Number Of Pokemon In Pokedex: " + user.getNumOfPokemon());
     }
+
+    /**
+     * Tried multiple things and could not figure out the button! Here is a cheat that I could just repeat 9 times
+     * that is incredibly wrong
+     */
     @FXML
     void viewNextPokemon()
     {
+        Pokedex user = new Pokedex("Daniel ");
+        Pokemon pokemon1 = new Pokemon("Dracozolt", "unknown",60,60,60,880);
+        Pokemon pokemon2 = new Pokemon("Copperajah", "both",80,80,50,879);
+        Pokemon pokemon3 = new Pokemon("Morpeko", "both",50,50,30,878);
+        Pokemon pokemon4 = new Pokemon("Morpeko", "both",40,60,40,877);
+        Pokemon pokemon5 = new Pokemon("Indeedee", "male",40,40,40,876);
+        Pokemon pokemon6 = new Pokemon("Eiscue", "both",50,50,70,875);
+        Pokemon pokemon7 = new Pokemon("Bulbasaur", "both",30,30,30,001);
+        Pokemon pokemon8 = new Pokemon("Ivysaur", "both",40,40,40,002);
+        Pokemon pokemon9 = new Pokemon("Venusaur", "both",50,50,50,003);
+        Pokemon pokemon10 = new Pokemon("Charmander", "both",30,40,30,004);
+
+        user.addPokemon(pokemon1);
+        user.addPokemon(pokemon2);
+        user.addPokemon(pokemon3);
+        user.addPokemon(pokemon4);
+        user.addPokemon(pokemon5);
+        user.addPokemon(pokemon6);
+        user.addPokemon(pokemon7);
+        user.addPokemon(pokemon8);
+        user.addPokemon(pokemon9);
+        user.addPokemon(pokemon10);
+
+
+        nameLabel.setText(pokemon2.getName());
+        numberLabel.setText(String.valueOf(pokemon2.getPokemonNumber()));
+        genderLabel.setText(pokemon2.getGender());
+        hPLabel.setText(String.valueOf(pokemon2.getHp()));
+        attackLabel.setText(String.valueOf(pokemon2.getAttack()));
+        defenseLabel.setText(String.valueOf(pokemon2.getDefense()));
 
     }
 
