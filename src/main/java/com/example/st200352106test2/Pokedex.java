4c20236e5752ea1cc5cb6d5473faf783aa3ded70
Daniel Humphreys <danielhumphreys@live.ca>
2022-02-24 12:52:46 -0500

    2 e) complete

--- src/main/java/com/example/st200352106test2/Pokedex.java
+++ src/main/java/com/example/st200352106test2/Pokedex.java
@@ -47,4 +47,43 @@ public class Pokedex {
         return pokemon.size();
     }
 
+    /**
+     * This method returns an arraylist of pokemon that have the gender matching the argument
+     * @param gender is the argument that will be used to compare to the array
+     * @return an array list of Pokemon that contain the same gender as the argument
+     */
+    public ArrayList<Pokemon> getPokemonWithGender(String gender)
+    {
+        ArrayList<Pokemon> array = new ArrayList<>();
+        for (Pokemon pokemon : pokemon)
+            if (pokemon.getGender().equals(gender))
+                array.add(pokemon);
+
+        return array;
+
+    }
+
+    /**
+     * Test class
+     * @param args
+     */
+    public static void main(String[] args) {
+        Pokedex user = new Pokedex("Daniel ");
+        Pokemon pokemon1 = new Pokemon("Dracozolt", "unknown",60,60,60,880);
+        Pokemon pokemon2 = new Pokemon("Copperajah", "both",80,80,50,879);
+        Pokemon pokemon3 = new Pokemon("Morpeko", "both",50,50,30,878);
+        Pokemon pokemon4 = new Pokemon("Morpeko", "both",40,60,40,877);
+        Pokemon pokemon5 = new Pokemon("Indeedee", "male",40,40,40,876);
+        Pokemon pokemon6 = new Pokemon("Eiscue", "both",50,50,70,875);
+        Pokemon pokemon7 = new Pokemon("Bulbasaur", "both",30,30,30,001);
+        Pokemon pokemon8 = new Pokemon("Ivysaur", "both",40,40,40,002);
+        Pokemon pokemon9 = new Pokemon("Venusaur", "both",50,50,50,003);
+        Pokemon pokemon10 = new Pokemon("Charmander", "both",30,40,30,004);
+
+        user.addPokemon(pokemon5);
+
+
+        System.out.println(user.getPokemonWithGender("male"));
+    }
+
 }
