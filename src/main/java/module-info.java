217989fad102a354a0d8b4ed5d2ca30b7ede8435
Daniel Humphreys <danielhumphreys@live.ca>
2022-02-24 11:12:33 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.st200352106test2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.st200352106test2 to javafx.fxml;
+    exports com.example.st200352106test2;
+}
