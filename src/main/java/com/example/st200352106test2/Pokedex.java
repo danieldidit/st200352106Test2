3756ac44ed5974edab44532dec0f76e13096521f
Daniel Humphreys <danielhumphreys@live.ca>
2022-04-14 12:31:39 -0400

    3 b) Completed

--- src/main/java/com/example/st200352106test2/Pokedex.java
+++ src/main/java/com/example/st200352106test2/Pokedex.java
@@ -7,8 +7,7 @@ public class Pokedex {
     private String ownersName;
     private ArrayList<Pokemon> pokemon;
 
-    public Pokedex (String ownersName)
-    {
+    public Pokedex(String ownersName) {
         setOwnersName(ownersName);
         pokemon = new ArrayList<>();
     }
@@ -23,37 +22,35 @@ public class Pokedex {
         ownersName = ownersName.trim();
 
         // Ensures the owners name is at least 3 characters in length
-        if(ownersName.matches("[A-Z][a-z][a-z][A-Za-z]*"))
+        if (ownersName.matches("[A-Z][a-z][a-z][A-Za-z]*"))
             this.ownersName = ownersName;
         else
             throw new IllegalArgumentException(" Owners name must beat least 3 characters in length");
     }
 
     /**
-     *Adds a new pokemon to the pokemon array list
+     * Adds a new pokemon to the pokemon array list
+     *
      * @param newPokemon adds a new Pokemon object to the Pokemon Array List
      */
-    public void addPokemon(Pokemon newPokemon)
-    {
+    public void addPokemon(Pokemon newPokemon) {
         pokemon.add(newPokemon);
     }
 
     /**
-     *
      * @return the number of Pokemon in the Pokemon Index
      */
-    public int getNumOfPokemon()
-    {
+    public int getNumOfPokemon() {
         return pokemon.size();
     }
 
     /**
      * This method returns an arraylist of pokemon that have the gender matching the argument
+     *
      * @param gender is the argument that will be used to compare to the array
      * @return an array list of Pokemon that contain the same gender as the argument
      */
-    public ArrayList<Pokemon> getPokemonWithGender(String gender)
-    {
+    public ArrayList<Pokemon> getPokemonWithGender(String gender) {
         ArrayList<Pokemon> array = new ArrayList<>();
         for (Pokemon pokemon : pokemon)
             if (pokemon.getGender().equals(gender))
@@ -65,16 +62,20 @@ public class Pokedex {
 
     /**
      * This method returns an array list of pokemon that have an attack equal to or greater than the minAttackValue
+     *
      * @param minAttackValue is the argument that will be used to compare tot he array
      * @return an array list of Pokemon that contain the same min attack value as the argument
      */
-    public ArrayList<Pokemon> getPokemonWithAttackValue(int minAttackValue)
-    {
+    public ArrayList<Pokemon> getPokemonWithAttackValue(int minAttackValue) {
         ArrayList<Pokemon> array = new ArrayList<>();
         for (Pokemon pokemon : pokemon)
             if (pokemon.getAttack() >= minAttackValue)
                 array.add(pokemon);
 
         return array;
+    }
+
+    public ArrayList<Pokemon> getPokemon() {
+        return pokemon;
     }
 }
--- src/main/java/com/example/st200352106test2/PokedexController.java
+++ src/main/java/com/example/st200352106test2/PokedexController.java
@@ -3,12 +3,17 @@ package com.example.st200352106test2;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class PokedexController implements Initializable {
+
+    Pokedex user;
+    int count = 0;
+
 
     @FXML
     private Label attackLabel;
@@ -34,17 +39,17 @@ public class PokedexController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        Pokedex user = new Pokedex("Daniel ");
-        Pokemon pokemon1 = new Pokemon("Dracozolt", "unknown",60,60,60,880);
-        Pokemon pokemon2 = new Pokemon("Copperajah", "both",80,80,50,879);
-        Pokemon pokemon3 = new Pokemon("Morpeko", "both",50,50,30,878);
-        Pokemon pokemon4 = new Pokemon("Morpeko", "both",40,60,40,877);
-        Pokemon pokemon5 = new Pokemon("Indeedee", "male",40,40,40,876);
-        Pokemon pokemon6 = new Pokemon("Eiscue", "both",50,50,70,875);
-        Pokemon pokemon7 = new Pokemon("Bulbasaur", "both",30,30,30,001);
-        Pokemon pokemon8 = new Pokemon("Ivysaur", "both",40,40,40,002);
-        Pokemon pokemon9 = new Pokemon("Venusaur", "both",50,50,50,003);
-        Pokemon pokemon10 = new Pokemon("Charmander", "both",30,40,30,004);
+        user = new Pokedex("Daniel ");
+        Pokemon pokemon1 = new Pokemon("Dracozolt", "unknown", 60, 60, 60, 880);
+        Pokemon pokemon2 = new Pokemon("Copperajah", "both", 80, 80, 50, 879);
+        Pokemon pokemon3 = new Pokemon("Morpeko", "both", 50, 50, 30, 878);
+        Pokemon pokemon4 = new Pokemon("Morpeko", "both", 40, 60, 40, 877);
+        Pokemon pokemon5 = new Pokemon("Indeedee", "male", 40, 40, 40, 876);
+        Pokemon pokemon6 = new Pokemon("Eiscue", "both", 50, 50, 70, 875);
+        Pokemon pokemon7 = new Pokemon("Bulbasaur", "both", 30, 30, 30, 001);
+        Pokemon pokemon8 = new Pokemon("Ivysaur", "both", 40, 40, 40, 002);
+        Pokemon pokemon9 = new Pokemon("Venusaur", "both", 50, 50, 50, 003);
+        Pokemon pokemon10 = new Pokemon("Charmander", "both", 30, 40, 30, 004);
 
         user.addPokemon(pokemon1);
         user.addPokemon(pokemon2);
@@ -57,55 +62,38 @@ public class PokedexController implements Initializable {
         user.addPokemon(pokemon9);
         user.addPokemon(pokemon10);
 
-
-        nameLabel.setText(pokemon1.getName());
-        numberLabel.setText(String.valueOf(pokemon1.getPokemonNumber()));
-        genderLabel.setText(pokemon1.getGender());
-        hPLabel.setText(String.valueOf(pokemon1.getHp()));
-        attackLabel.setText(String.valueOf(pokemon1.getAttack()));
-        defenseLabel.setText(String.valueOf(pokemon1.getDefense()));
 
         numberOfPokemonLabel.setText("Number Of Pokemon In Pokedex: " + user.getNumOfPokemon());
+
+        System.out.println(user.toString());
+
+        nameLabel.setText(user.getPokemon().get(count).getName());
+        numberLabel.setText(String.valueOf(user.getPokemon().get(count).getPokemonNumber()));
+        genderLabel.setText(user.getPokemon().get(count).getGender());
+        hPLabel.setText(String.valueOf(user.getPokemon().get(count).getHp()));
+        attackLabel.setText(String.valueOf(user.getPokemon().get(count).getAttack()));
+        defenseLabel.setText(String.valueOf(user.getPokemon().get(count).getDefense()));
     }
 
-    /**
-     * Tried multiple things and could not figure out the button! Here is a cheat that I could just repeat 9 times
-     * that is incredibly wrong
-     */
+
     @FXML
-    void viewNextPokemon()
-    {
-        Pokedex user = new Pokedex("Daniel ");
-        Pokemon pokemon1 = new Pokemon("Dracozolt", "unknown",60,60,60,880);
-        Pokemon pokemon2 = new Pokemon("Copperajah", "both",80,80,50,879);
-        Pokemon pokemon3 = new Pokemon("Morpeko", "both",50,50,30,878);
-        Pokemon pokemon4 = new Pokemon("Morpeko", "both",40,60,40,877);
-        Pokemon pokemon5 = new Pokemon("Indeedee", "male",40,40,40,876);
-        Pokemon pokemon6 = new Pokemon("Eiscue", "both",50,50,70,875);
-        Pokemon pokemon7 = new Pokemon("Bulbasaur", "both",30,30,30,001);
-        Pokemon pokemon8 = new Pokemon("Ivysaur", "both",40,40,40,002);
-        Pokemon pokemon9 = new Pokemon("Venusaur", "both",50,50,50,003);
-        Pokemon pokemon10 = new Pokemon("Charmander", "both",30,40,30,004);
+    void viewNextPokemon() {
+        try {
 
-        user.addPokemon(pokemon1);
-        user.addPokemon(pokemon2);
-        user.addPokemon(pokemon3);
-        user.addPokemon(pokemon4);
-        user.addPokemon(pokemon5);
-        user.addPokemon(pokemon6);
-        user.addPokemon(pokemon7);
-        user.addPokemon(pokemon8);
-        user.addPokemon(pokemon9);
-        user.addPokemon(pokemon10);
+            count++;
 
+            if (count > 9) {
+                throw new IllegalArgumentException("End of pokemon");
+            }
 
-        nameLabel.setText(pokemon2.getName());
-        numberLabel.setText(String.valueOf(pokemon2.getPokemonNumber()));
-        genderLabel.setText(pokemon2.getGender());
-        hPLabel.setText(String.valueOf(pokemon2.getHp()));
-        attackLabel.setText(String.valueOf(pokemon2.getAttack()));
-        defenseLabel.setText(String.valueOf(pokemon2.getDefense()));
-
+            nameLabel.setText(user.getPokemon().get(count).getName());
+            numberLabel.setText(String.valueOf(user.getPokemon().get(count).getPokemonNumber()));
+            genderLabel.setText(user.getPokemon().get(count).getGender());
+            hPLabel.setText(String.valueOf(user.getPokemon().get(count).getHp()));
+            attackLabel.setText(String.valueOf(user.getPokemon().get(count).getAttack()));
+            defenseLabel.setText(String.valueOf(user.getPokemon().get(count).getDefense()));
+        } catch (IllegalArgumentException exception) {
+            numberOfPokemonLabel.setText(exception.getMessage());
+        }
     }
-
 }
